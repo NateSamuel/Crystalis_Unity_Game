6d3f9689527ad17e8342eff5140698fbce9e4b06
MSI\nateb <nate.bentley@outlook.com>
2025-06-13 18:37:39 +0100

    additional items

--- Assets/Scripts/CharacterTreasure.cs
+++ Assets/Scripts/CharacterTreasure.cs
@@ -1,12 +1,22 @@
 using UnityEngine;
+using TMPro;
 
 public class CharacterTreasure : MonoBehaviour
 {
     
     int crystals = 0;
+    public TextMeshProUGUI treasureText;
 
     public void ApplyTreasure()
     {
         crystals += 2;
+        UpdateTreasureUI();
+    }
+    void UpdateTreasureUI()
+    {
+        if (treasureText != null)
+        {
+            treasureText.text = "Crystals: " + crystals;
+        }
     }
 }
